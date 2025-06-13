0786ae88c8838f655e7f6c2a524681037e0d6632
Ishika Bhatt <ishikap1998@gmail.com>
2025-06-13 09:52:49 +0530

    Update main.tf

--- DevOps/Terraform/project/modules/vpc/main.tf
+++ DevOps/Terraform/project/modules/vpc/main.tf
@@ -5,11 +5,4 @@ resource "aws_vpc" "main" {
     Name = var.vpc_name
   }
 }
-root@ip-172-31-42-50:~/CDEC-B52/DevOps/Terraform/project/modules/vpc# cat main.tf 
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
 
-  tags = {
-    Name = var.vpc_name
-  }
-}
